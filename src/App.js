017a8474124e48dbe4bba845ca23526c4be819b2
Mohammad-Ashraf09 <mohd.ashraf@tothenew.com>
2022-03-07 01:15:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let dummy_expenses = [
+    {
+        id: 'e1',
+        title: 'School Fee',
+        amount: 500,
+        date: new Date(2021, 5, 12)
+    },
+];
+
+const App = () =>
+{
+    const [expenses, setExpenses] = useState(dummy_expenses);  // useState returns an array which contain two elements one array and other is a function.
+                                                               // And we destructured that returned array. expenses as variable and setExpenses as function which is used for change the title
+
+    const addExpenseHandler = (expense) =>{
+        const updatedExpense = [expense, ...expenses];
+        setExpenses(updatedExpense);
+    };
+
+    return(
+        <div>
+            <NewExpense onAddExpense={addExpenseHandler}/>
+            <Expenses items={expenses} />
+        </div>
+    );
 }
 
 export default App;
